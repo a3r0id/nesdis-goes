a40967a97c12777fbeb318f05186a02716351a7e
Host-Info.net <50676042+hostinfodev@users.noreply.github.com>
2020-10-15 13:03:22 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from NesdisAPI import nesdis
-  
+# [**i**] LIBRARY TO GRACEFULLY SCRAPE PUBLIC SATELLITE IMAGERY FROM https://star.nesdis.noaa.gov [**i**]
+# Python API By github.com/hostinfodev  
 
 
 
